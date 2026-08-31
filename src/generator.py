fd7f4655aaadd544ad3304bc80fe990293752064
DRISHYA GARG <drishyagarg22@gmail.com>
2026-08-31 12:18:21 +0000

    Creating payment simulations

---
+++ src/generator.py
@@ -0,0 +1,32 @@
+PAYMENT_METHODS = [
+    "upi",
+    "credit_card",
+    "debit_card",
+    "net_banking",
+    "wallet"
+]
+
+BANKS = [
+    "bank_a",
+    "bank_b",
+    "bank_c",
+    "bank_d",
+    "bank_e",
+    "bank_f"
+]
+
+PROVIDERS = [
+    "provider_x",
+    "provider_y",
+    "provider_z"
+]
+
+GEOGRAPHIES = [
+    "delhi_ncr",
+    "mumbai",
+    "bengaluru",
+    "hyderabad",
+    "chennai",
+    "kolkata",
+    "pune"
+]
